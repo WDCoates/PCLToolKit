7c7e9132b14d8df80c0e7805114843a6228e5df3
DCoates <DCoates@Rubiconsoftware.com>
2019-07-12 08:57:54 +0100

    Initial commit

---
+++ BoolToColourConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+
+namespace PCLToolKit
+{
+    public class BoolToColourConverter : IValueConverter    
+    {
+        public Color TrueColour { get; set; }
+        public Color FalseColour { get; set; }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool) value ? TrueColour : FalseColour;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return false;
+        }
+    }
+}
---
+++ Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PCLToolKit
+{
+    public class Class1
+    {
+    }
+}
